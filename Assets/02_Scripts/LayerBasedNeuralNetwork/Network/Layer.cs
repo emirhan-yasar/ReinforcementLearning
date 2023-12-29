fcb6fc888a79ebba25a8979d2eedaade7a36c8f0
Mustafaysrs <mstfysr2001@gmail.com>
2023-12-29 10:09:08 +0300

    ComputeShader

--- Assets/02_Scripts/LayerBasedNeuralNetwork/Network/Layer.cs
+++ Assets/02_Scripts/LayerBasedNeuralNetwork/Network/Layer.cs
@@ -1,4 +1,6 @@
-﻿namespace LayerBasedNeuralNetwork
+﻿using UnityEngine;
+
+namespace LayerBasedNeuralNetwork.Network
 {
     public class Layer
     {
@@ -7,13 +9,14 @@
         private readonly float[,] _weights;
         private readonly float[] _biases;
 
-        public Layer(uint numNeuronsIn, uint numNeuronsOut)
+        public Layer(uint numNeuronsIn, uint numNeuronsOut, ComputeShader layerComputeShader)
         {
             _numNeuronsIn = numNeuronsIn;
             _numNeuronsOut = numNeuronsOut;
 
             _weights = new float[numNeuronsOut,numNeuronsIn]; // numOfRows = numNeuronsOut, numOfColumns = numNeuronsIn
             _biases = new float[numNeuronsOut];
+            RandomlyInitializeLayer();
         }
 
         public float[] CalculateOutputs(float[] inputs)
@@ -32,6 +35,20 @@
             }
 
             return layerOutputs;
+        }
+
+        private void RandomlyInitializeLayer()
+        {
+            for (var row = 0; row < _weights.GetLength(0); row++)
+            for (var column = 0; column < _weights.GetLength(1); column++)
+            {
+                _weights[row, column] = Random.Range(-2f, 2f);
+            }
+
+            for (var i = 0; i < _biases.Length; i++)
+            {
+                _biases[i] = Random.Range(-2f, 2f);
+            }
         }
 
     }
--- Assets/02_Scripts/LayerBasedNeuralNetwork/Network/NeuralNetwork.cs
+++ Assets/02_Scripts/LayerBasedNeuralNetwork/Network/NeuralNetwork.cs
@@ -1,17 +1,19 @@
-﻿namespace LayerBasedNeuralNetwork
+﻿using UnityEngine;
+
+namespace LayerBasedNeuralNetwork.Network
 {
     public class NeuralNetwork
     {
         private readonly Layer[] _layers;
 
-        public NeuralNetwork(params uint[] layerSizes)
+        public NeuralNetwork(ComputeShader layerComputeShader, params uint[] layerSizes)
         {
             // Not include input layer
             _layers = new Layer[layerSizes.Length - 1];
 
             for (var i = 0; i < _layers.Length; i++)
             {
-                _layers[i] = new Layer(layerSizes[i], layerSizes[i + 1]);
+                _layers[i] = new Layer(layerSizes[i], layerSizes[i + 1], layerComputeShader);
             }
         }
 
